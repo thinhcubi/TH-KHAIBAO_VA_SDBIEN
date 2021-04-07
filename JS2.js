4fea39fabf338ee87af1cf00dfc0cd652235a68f
thine <thinhcubi27@gmail.com>
2021-04-07 11:09:04 +0700

    JS2.js

---
+++ JS2.js
@@ -0,0 +1,11 @@
+let i = 10
+document.write('i = ' + i)
+document.write('<br/>')
+let f = 20.5
+document.write('f =  ' + f)
+document.write('<br/>')
+let b = true
+document.write('b = '  + b)
+document.write('<br/>')
+let s = 'Hà Nội'
+document.write('s =  ' + s)
